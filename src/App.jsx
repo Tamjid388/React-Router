e7e109ef880c7e6793525970f302cae39183da6d
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2024-10-28 01:20:41 +0600

    completed  poject setup

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+ <Header></Header>
+     
+   
+    </>
+  )
+}
+
+
+function Header(){
+  return(
+    <p className='btn'>aaaaa</p>
+  )
+}
+
+
+export default App
